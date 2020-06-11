9d5fa5145ffd4353ea1629c34137d36fce7dd430
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-06-11 12:29:58 -0400

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'graph-webpage';
+  title = 'Sample Text';
+  imgSrc = '../assets/original.png';
+  hoverSrc = '../assets/hover.png';
+  hover2Src = '../assets/hover2.png';
+  graph1 = '../assets/graph1.png';
+  graph1Hover = '../assets/graph1_hover.png';
+  graph2 = '../assets/graph2.png';
+  graph2Hover = '../assets/graph2_hover.png';
+  // graph3 = '../assets/graph3.png';
+  // graph3Hover = '../assets/graph3_hover.png';
+  // graph4 = '../assets/graph4.png';
+  // graph4Hover = '../assets/graph4_hover.png';
+  // graph5 = '../assets/graph5.png';
+  // graph5Hover = '../assets/graph5_hover.png';
+
+
+  // onMouseOver(): void {
+  //   this.imgSrc = '../assets/hover.png';
+  // }
+  //
+  // onMouseOut(): void {
+  //   this.imgSrc = '../assets/original.png';
+  // }
+  //
+  // onMouseOver2(): void {
+  //   this.imgSrc = '../assets/hover.png';
+  // }
+  //
+  // onMouseOut2(): void {
+  //   this.imgSrc = '../assets/original.png';
+  // }
+
+
 }
