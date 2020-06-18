cf8510aff67e971f824b0a04e6fa0ca3864880a4
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-06-18 10:30:13 -0400

    Xs v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,12 @@ export class AppComponent {
   graph1 = '../assets/graph1.png';
   graph1Hover = '../assets/graph1_hover.png';
   graph2 = '../assets/graph2.png';
-  graph2Hover = '../assets/graph2_hover.png';
-  // graph3 = '../assets/graph3.png';
+  graph2Hover = '../assets/graph2_hover_test.png';
+  graph3 = '../assets/graph3.png';
   // graph3Hover = '../assets/graph3_hover.png';
-  // graph4 = '../assets/graph4.png';
+  graph4 = '../assets/graph4.png';
   // graph4Hover = '../assets/graph4_hover.png';
-  // graph5 = '../assets/graph5.png';
+  graph5 = '../assets/graph5.png';
   // graph5Hover = '../assets/graph5_hover.png';
 
 
